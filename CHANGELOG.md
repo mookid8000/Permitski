6e43cdf4bf8f609f0bfd5722b5144caadb1d738a
Mogens Heller Grabe <mookid8000@gmail.com>
2023-03-11 19:58:11 +0100

    add test case

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,17 +1,16 @@
 # Changelog
 
 ## 0.0.1
-
 * Made Permitski
 
 ## 0.0.3
-
 * Compact and non-compact format
 
 ## 0.0.4
-
 * Make it possible to use single-quotes in default output
 
 ## 0.0.5
-
 * Add deserializer
+
+## 0.0.6
+* Update Newtonsoft dependency to 13.0.3
---
+++ Permitski.Tests/TestInvariants.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using Testy;
+
+namespace Permitski.Tests;
+
+[TestFixture]
+public class TestInvariants : FixtureBase
+{
+    [Test]
+    [Description("Verifies that a JSON-serialized complex object results in the exact same JSON output as it did when Permitski was initially made. This is important, because JSON is the basis of the signature, so it must be precise down to the last bit.")]
+    public void TestJsonSerializationOfComplexObject()
+    {
+        var doc = new RootDocument("HEJ MED DIG", new[]
+        {
+            new ChildDocument("HEJ IGEN 1", new[] {1, 2, 3}),
+            new ChildDocument("HEJ IGEN 2", new[] {1, 2, 3, 4, 5}),
+            new ChildDocument("HEJ IGEN 3", new[] {1, 2, 3, 4, 5, 6, 7})
+        });
+
+        var json = JsonConvert.SerializeObject(doc);
+
+        Console.WriteLine(json);
+
+        Assert.That(json, Is.EqualTo(@"{""Text"":""HEJ MED DIG"",""Children"":[{""AnotherText"":""HEJ IGEN 1"",""ListOfIntegers"":[1,2,3]},{""AnotherText"":""HEJ IGEN 2"",""ListOfIntegers"":[1,2,3,4,5]},{""AnotherText"":""HEJ IGEN 3"",""ListOfIntegers"":[1,2,3,4,5,6,7]}]}"));
+    }
+
+    record RootDocument(string Text, IReadOnlyList<ChildDocument> Children);
+
+    record ChildDocument(string AnotherText, IReadOnlyList<int> ListOfIntegers);
+}
